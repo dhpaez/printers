b7f179dd193e713d107aea3df170efbc463842c4
Diego Paez <dhpaez@gmail.com>
2022-12-19 12:48:14 -0300

    update

--- js/mensaje.js
+++ js/mensaje.js
@@ -1,2 +1,9 @@
-document.write('Mensaje de prueba!!');
+//alert('Mensaje de prueba!!');
 //onclick="alert('Prueba');"
+//document.write(new Date());
+function mapprint(){
+    window.FileSystem('file://principal/epsontx115');
+}
+function messaje(){
+    alert(Date());
+}
